60f41aed1771c0be80329bf3640b0a30d3a993b0
Y.K. Goon <ykgoon@gmail.com>
2017-01-31 16:14:29 +0800

    Initialize data on spreadsheet

--- app/dev/js/out/app_front/core.cljs
+++ app/dev/js/out/app_front/core.cljs
@@ -1,16 +1,20 @@
 (ns app-front.core
   (:require cljsjs.handsontable))
 
-(defonce app-state (atom {:message "Xecle hello world"}))
+(defonce app-state (atom {:message "Xecle: rethinking spreadsheet"}))
 
 (defn mount-root [setting]
   (let [app (. js/document (getElementById "app"))
-        spreadsheet [["Sed" "Mauris" "Nam"]
-                     [4 34 59]
-                     [8 4 43]
-                     [9 2 10]]]
-    (js/Handsontable app spreadsheet)))
-    ;; (set! (.-innerHTML app) spreadsheet)))
+        spreadsheet (array
+                     (array "Sed" "Mauris" "Nam")
+                     (array 4 34 59)
+                     (array 8 4 43)
+                     (array 9 2 10))
+        table-options (js-obj "data" spreadsheet
+                              "rowHeader" true
+                              "colHeader" true
+                              "dropdownMenu" true)]
+    (js/Handsontable app table-options)))
 
 (defn init! [setting]
   (mount-root setting))
--- app/dev/js/out/app_front/core.js
+++ app/dev/js/out/app_front/core.js
@@ -4,12 +4,15 @@ goog.require('cljs.core');
 goog.require('cljsjs.handsontable');
 if(typeof app_front.core.app_state !== 'undefined'){
 } else {
-app_front.core.app_state = cljs.core.atom.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"message","message",-406056002),"Xecle hello world"], null));
+app_front.core.app_state = cljs.core.atom.call(null,new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"message","message",-406056002),"Xecle: rethinking spreadsheet"], null));
 }
 app_front.core.mount_root = (function app_front$core$mount_root(setting){
 var app = document.getElementById("app");
-var spreadsheet = new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, ["Sed","Mauris","Nam"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(4),(34),(59)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(8),(4),(43)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(9),(2),(10)], null)], null);
-return Handsontable(app,spreadsheet);
+var spreadsheet = [["Sed","Mauris","Nam"],[(4),(34),(59)],[(8),(4),(43)],[(9),(2),(10)]];
+var table_options = (function (){var obj20903 = {"data":spreadsheet,"rowHeader":true,"colHeader":true,"dropdownMenu":true};
+return obj20903;
+})();
+return Handsontable(app,table_options);
 });
 app_front.core.init_BANG_ = (function app_front$core$init_BANG_(setting){
 return app_front.core.mount_root.call(null,setting);
